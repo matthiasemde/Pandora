746eb1c7731c536010b0f137c287dbf95264b43b
Matthias Emde <matthias@emdemail.de>
2025-05-26 20:23:40 +0200

    feat: add some basic pkgs to mahler

--- hosts/mahler/configuration.nix
+++ hosts/mahler/configuration.nix
@@ -81,6 +81,9 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    bat
+    btop
+    neofetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
