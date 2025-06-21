1ae6d62f6500e50dcc30c3cffd408859694b6a12
Matthias Emde <matthias@emdemail.de>
2025-06-21 15:07:09 +0200

    feat: add macvlan network for docker This is done in preparation for the installation of AdGuard which will use this network in order to avoid port conflicts with the host.

--- virtualization/configuration.nix
+++ virtualization/configuration.nix
@@ -9,4 +9,26 @@ in {
     backend    = "docker";
     containers = mergedContainers;
   };
+
+  # Declare a custom docker network fhich can be used by services like adguard
+  systemd.services.macvlan-network = {
+    description = "Create macvlan Docker network for LAN bridging";
+    wantedBy = [ "docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "create-macvlan" ''
+        ${pkgs.docker}/bin/docker network inspect macvlan0 >/dev/null 2>&1 || \
+          ${pkgs.docker}/bin/docker network create -d macvlan \
+            --subnet=192.168.178.0/24 \
+            --gateway=192.168.178.1 \
+            --ip-range=192.168.178.240/28 \
+            --ipv6 \
+            --subnet=fdfb:7759:b7ce::/64 \
+            --gateway=fdfb:7759:b7ce::2e91:abff:fea2:270e \
+            -o parent=enp106s0f3u2 \
+            macvlan0
+      '';
+      RemainAfterExit = true;
+    };
+  };
 }
